3437d3da77e8796f167a7492908ce4138412e111
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-10-24 18:54:37 +0300

    change type of services from their implementations to interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import model.InputFileInfo;
+import service.AnswerGeneratorService;
+import service.ParserService;
+import service.ReaderService;
+import service.WriterService;
 import service.impl.AnswerGeneratorServiceImpl;
 import service.impl.ParserServiceImpl;
 import service.impl.ReaderServiceImpl;
@@ -6,16 +10,16 @@ import service.impl.WriterServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        ReaderServiceImpl reader = new ReaderServiceImpl();
+        ReaderService reader = new ReaderServiceImpl();
         String inputFileContent = reader.read("input.txt");
 
-        ParserServiceImpl parser = new ParserServiceImpl();
+        ParserService parser = new ParserServiceImpl();
         InputFileInfo inputFileInfo = parser.parse(inputFileContent);
 
-        AnswerGeneratorServiceImpl answerGenerator = new AnswerGeneratorServiceImpl();
+        AnswerGeneratorService answerGenerator = new AnswerGeneratorServiceImpl();
         String answer = answerGenerator.generate(inputFileInfo);
 
-        WriterServiceImpl writer = new WriterServiceImpl();
+        WriterService writer = new WriterServiceImpl();
         writer.write(answer, "output.txt");
     }
 }
